2d37f284d58f14bf7024023a68f34aafe12d4679
ankitpanchaal <54738307+ankitpanchaal@users.noreply.github.com>
2023-08-30 16:03:16 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -10,35 +10,37 @@ export default function App() {
 
   return (
     <div
-      // style={containerStyles}
       ref={containerRef}
       style={{ height: "100vh", width: "100wh" }}
       className="bg-slate-400 items-center justify-center flex w-screen h-screen flex-wrap overflow-scroll"
     >
       <Tree
-        data={orgChartJson}
-        translate={translate}
-        orientation="vertical"
-        draggable={true}
-        zoomable={true}
-        initialDepth={1}
-        // dimensions={true}
-        enableLegacyTransitions={1000}
-        separation={{ siblings: 2, nonSiblings: 2 }}
+      data={orgChartJson}
+      translate={translate}
+      orientation="vertical"
+      draggable={true}
+      zoomable={true}
+      initialDepth={1}
+      // dimensions={true}
+      enableLegacyTransitions={1000}
+      renderCustomNodeElement={renderRectSvgNode}
+      centeringTransitionDuration={300}
+      separation={{ siblings: 2, nonSiblings: 2 }}
+      pathFunc="step"
       />
     </div>
   );
 }
 
 const renderRectSvgNode = ({ nodeDatum, toggleNode }) => (
-  <g>
-    <circle r="10" onClick={toggleNode} />
-    <text fill="black" strokeWidth="1" x="20">
+  <g onClick={toggleNode}>
+    <rect width="20" height="20" x="-10" fill="#ffeab0" rx={33} />
+    <text fill="black" strokeWidth="1" x="20" fontSize={14}>
       {nodeDatum.name}
     </text>
-    {nodeDatum.attributes?.department && (
-      <text fill="black" x="20" dy="20" strokeWidth="1">
-        Department: {nodeDatum.attributes?.department}
+    {nodeDatum.value && (
+      <text fill="black" x="20" dy="20" strokeWidth="0" fontSize={12}>
+        Value: {nodeDatum.value}
       </text>
     )}
   </g>
